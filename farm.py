b21ec47fc9a08c242ecc2f1fb972c5592ea92646
Lcky <no@email.com>
2022-06-17 18:05:57 -0400

    broke out basic functions; log in working

--- farm.py
+++ farm.py
@@ -1,31 +1,70 @@
 # Imports
 from selenium import webdriver
-import time
-import config
+from selenium.webdriver.common.by import By
+from time import sleep
+# Comment this out
+import secret_config as config
+# Uncomment this out and fill with your login information
+#import config
 
 # Config
 USERNAME = config.USER_NAME
 PASSWORD = config.USER_PASS
-URL = 'https://farmrpg.com/#!/login.php'
+GAME_URL = "https://farmrpg.com/"
+LOGIN_URL = "https://farmrpg.com/#!/login.php"
+LOGOUT_URL = "logout.php"
+GAME_TITLE = "Farm RPG"
 
-# Selenium Config
-driver = webdriver.Chrome('C:/browser_drivers/chromedriver.exe')
-
-# Login
+# Selenium Setup
+driver = webdriver.Chrome("C:/browser_drivers/chromedriver.exe")
 driver.set_window_size(800, 800)
-time.sleep(1)
+driver.get(GAME_URL)
+sleep(.5)
 
-driver.get(URL)
-time.sleep(1)
+# Quit Web Driver
+def tearDown():
+    driver.quit()
 
-driver.find_element_by_name('username').send_keys(USERNAME)
-driver.find_element_by_name('password').send_keys(PASSWORD)
-time.sleep(1)
+# Get Tab Title
+def getTitle():
+    return driver.title
 
-driver.find_element_by_id('login_sub').click()
+# Check if game is already logged in
+def checkLogin():
+    try:
+        driver.find_element(by=By.XPATH, value='//a[@href="'+LOGOUT_URL+'"]')
+        print("User is logged in!")
+        return True
+    except:
+        print("User is NOT logged in!")
+        return False
+
+# Log the user in
+def login():
+    if checkLogin():
+        pass
+    else:
+        driver.get(LOGIN_URL)
+        sleep(.5)
+        driver.refresh()
+        sleep(.5)
+
+        assert getTitle() == GAME_TITLE
+
+        driver.find_element_by_name("username").send_keys(USERNAME)
+        driver.find_element_by_name("password").send_keys(PASSWORD)
+        sleep(.5)
+
+        driver.find_element_by_id("login_sub").click()
+        sleep(.5)
+        if checkLogin() == False:
+            print("!!! Failed to log in !!!")
+            tearDown()
 
 
 
-# For testing
-time.sleep(5)
-driver.close()
+
+login()
+# End
+sleep(5)
+tearDown()
